25b91c9f3cf5032b35ce823b34bb05d03cf0066a
Pavel Kladivák <tglozar@gmail.com>
2017-10-04 15:55:20 +0200

    Added serial output support (Unix only)

---
+++ src/main/java/avrconfig/serial/ISerialController.java
@@ -0,0 +1,7 @@
+package avrconfig.serial;
+
+/**
+ * Created by tglozar on 27.8.17.
+ */
+public interface ISerialController {
+}
---
+++ src/main/java/avrconfig/serial/Serial.java
@@ -0,0 +1,7 @@
+package avrconfig.serial;
+
+/**
+ * Created by tglozar on 20.8.17.
+ */
+public class Serial {
+}
