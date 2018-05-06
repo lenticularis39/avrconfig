cc6ed40af9d4bcc38cb090020b7cb85bb4459986
Pavel Kladivák <tglozar@gmail.com>
2018-05-06 09:37:51 +0200

    Moved version.

--- src/main/java/avrconfig/Main.java
+++ src/main/java/avrconfig/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
             configurationFile = getParameters().getNamed().get("config");
         }
         Parent root = fxmlLoader.load();
-        primaryStage.setTitle("AVRConfig");
+        primaryStage.setTitle("AVRConfig 0.2-beta");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
