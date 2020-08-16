fe2c8b67f6cdea54cd571fa7bbed984a932eb241
Tomáš Glozar <tglozar@gmail.com>
2020-08-16 18:24:06 +0200

    Show version number based on manifest.

--- src/main/java/avrconfig/Main.java
+++ src/main/java/avrconfig/Main.java
@@ -1,42 +1,38 @@
 package avrconfig;
 
-import avrconfig.data.ConfigurationFile;
-import avrconfig.error.ErrorHandler;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class Main extends Application {
-
     FXMLLoader fxmlLoader;
     String configurationFile;
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("main.fxml"));
 
-        // Configuration file needs to be set up to load before the FXML is loaded
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the configuration file.
+        // Note: this has to be done before the FXML is loaded.
         configurationFile = System.getProperty("user.home") + "/.config/avrconfig/config.xml";
-        if(getParameters().getNamed().get("config") != null) {
+        if (getParameters().getNamed().get("config") != null) {
             System.out.println("Setting configuration file.");
             configurationFile = getParameters().getNamed().get("config");
         }
+
+        // Determine AVRConfig version.
+        // Note: when running outside JAR file (typically during development), the manifest is not present, therefore
+        // the version number is unknown.
+        String version = "(development version)";
+        if (getClass().getPackage().getImplementationVersion() != null)
+            version = getClass().getPackage().getImplementationVersion();
+
+        // Load the FXML and show the main window.
+        fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("main.fxml"));
         Parent root = fxmlLoader.load();
-        primaryStage.setTitle("AVRConfig 0.2-beta");
+        primaryStage.setTitle("AVRConfig " + version);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-    }
-
-    @Override
-    public void stop() {
-
     }
 
     public static void main(String[] args) {
